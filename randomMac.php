212aa5ca31437e1937a94998aee351f437dcaf8f
Marco Kraft <69326651+MaKraMc@users.noreply.github.com>
2022-05-12 19:10:39 +0200

    Create randomMac.php

---
+++ randomMac.php
@@ -0,0 +1,28 @@
+function randomMac()
+{
+    $macArray[0] = "A";
+    $macArray[1] = "B";
+    $macArray[2] = "C";
+    $macArray[3] = "D";
+    $macArray[4] = "E";
+    $macArray[5] = "F";
+    $macArray[6] = "0";
+    $macArray[7] = "1";
+    $macArray[8] = "2";
+    $macArray[9] = "3";
+    $macArray[10] = "4";
+    $macArray[11] = "5";
+    $macArray[12] = "6";
+    $macArray[13] = "7";
+    $macArray[14] = "7";
+    $macArray[15] = "9";
+
+    $randomMac =
+        $macArray[random_int(0, 15)] . $macArray[random_int(0, 15)] . ":" .
+        $macArray[random_int(0, 15)] . $macArray[random_int(0, 15)] . ":" .
+        $macArray[random_int(0, 15)] . $macArray[random_int(0, 15)] . ":" .
+        $macArray[random_int(0, 15)] . $macArray[random_int(0, 15)] . ":" .
+        $macArray[random_int(0, 15)] . $macArray[random_int(0, 15)] . ":" .
+        $macArray[random_int(0, 15)] . $macArray[random_int(0, 15)];
+    return $randomMac;
+}
