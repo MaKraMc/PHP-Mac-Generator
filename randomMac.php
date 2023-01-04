bb71eb42f2582ea895e3e0966b9556874f12df16
MaKraMc <marco77755.mk@gmail.com>
2023-01-04 20:53:54 +0100

    optimize code

--- randomMac.php
+++ randomMac.php
@@ -2,22 +2,7 @@
 
 function randomMac(string $delimeter = ":")
 {
-    $macArray[0] = "A";
-    $macArray[1] = "B";
-    $macArray[2] = "C";
-    $macArray[3] = "D";
-    $macArray[4] = "E";
-    $macArray[5] = "F";
-    $macArray[6] = "0";
-    $macArray[7] = "1";
-    $macArray[8] = "2";
-    $macArray[9] = "3";
-    $macArray[10] = "4";
-    $macArray[11] = "5";
-    $macArray[12] = "6";
-    $macArray[13] = "7";
-    $macArray[14] = "7";
-    $macArray[15] = "9";
+    $macArray = array("A", "B", "C", "D", "E", "F", "0", "1", "2", "3", "4", "5", "6", "7", "7", "9");
 
     $randomMac =
         $macArray[random_int(0, 15)] . $macArray[random_int(0, 15)] . $delimeter .
